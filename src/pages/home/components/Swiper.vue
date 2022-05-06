c19b39c93a437412b1c45908da4674bb8e95c4af
r2987183 <r2987183@yahoo.com.tw>
2022-05-06 16:47:17 +0800

    main

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -10,13 +10,6 @@
             </swiper-slide>
             <swiper-slide>
                 <img class="swiper-img" src="https://www.ysdet.tw/images/banner/Banner01.jpg" />
-            </swiper-slide>
-            <swiper-slide>
-                <img class="swiper-img" src="https://www.ysdet.tw/images/banner/Banner01.jpg" />
-            </swiper-slide>
-            <swiper-slide>
-                <img class="swiper-img" src="https://www.ysdet.tw/images/banner/Banner01.jpg" />
-            </swiper-slide>
             <!-- Optional controls -->
             <div class="swiper-pagination"  slot="pagination"></div>
         </swiper>
